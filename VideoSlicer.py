f5c706eaadccecc66729b17bb30ad725482f94ca
brendannagle1 <48375015+brendannagle1@users.noreply.github.com>
2020-01-29 23:54:17 -0800

    Update VideoSlicer.py
    Fixed the fps frame reducer bug

--- VideoSlicer.py
+++ VideoSlicer.py
@@ -7,22 +7,24 @@ def loadvideo(fullPath, fps, interval):
     print('Frames Loading')
     vidcap = cv2.VideoCapture(fullPath)
     readfps = vidcap.get(cv2.CAP_PROP_FPS)
+    print(f'Video FPS:{readfps} Requested FPS {fps}')
     # print(readfps)
     success, image = vidcap.read()
     
-    multiplier = int(readfps/ fps)
+    multiplier = round(readfps/ fps,0)
+    print(f'Multiplier:{multiplier}')
 
     while success:
         frameId = int(round(vidcap.get(1)))
         success, image = vidcap.read()
         interval = interval + 1
 
-        if frameId % multiplier == 0:
+        if (frameId == 0) or (frameId % multiplier == 0):
             images.append(image)
     
     vidcap.release()
 
-    return images, readfps, interval
+    return images, readfps, interval, multiplier
 
 # https://theailearner.com/2018/10/15/creating-video-from-images-using-opencv-python/
 def savevideo(list, folderPath, vidname, suf, fps):
@@ -59,13 +61,13 @@ def main():
 
     interval = 0
     readfps = 1
-    fps = 30
+    fps = 15
 
     frame_select_tb_name = 'Frm Sel'
     window_capture_name = 'Video Capture'
 
 
-    images, readfps, interval = loadvideo(fullPath, fps, interval)
+    images, readfps, interval, multiplier = loadvideo(fullPath, fps, interval)
 # savevideo(images)
 
     print(f"Image Count:{len(images)}")
@@ -83,7 +85,7 @@ def main():
 
     while True:
     
-        key = cv2.waitKeyEx(30)
+        key = cv2.waitKeyEx(30) #esc key
         if key == ord('q') or key == 27:
             break
 
@@ -123,11 +125,12 @@ def main():
             trimed_frames_coords.sort()
             print(f'Min Frame:{trimed_frames_coords[0]}, Max Frame:{trimed_frames_coords[1]}')
             cv2.destroyWindow(window_capture_name)
-            savevideo(images[trimed_frames_coords[0]:trimed_frames_coords[1]],folderPath, vidname, suf, readfps) 
+            savevideo(images[trimed_frames_coords[0]:trimed_frames_coords[1]],folderPath, vidname, suf, readfps/multiplier) 
             break
 
-        cv2.imshow(window_capture_name,images[value])
+        cv2.imshow(window_capture_name, images[value])
 
 if __name__ == '__main__':
 
     main()
+
